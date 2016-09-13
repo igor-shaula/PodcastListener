9891e2bf3c918aacb185e9b4dae053a6b5c3235d
igor-shaula <igor.shaula@gmail.com>
2016-09-13 13:06:47 +0300

    project started

---
+++ app/src/main/java/i_will_pass/to_the_final_of/devchallenge_x/MainActivity.java
@@ -0,0 +1,13 @@
+package i_will_pass.to_the_final_of.devchallenge_x;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
